3cdd0b1b66ebab24eee20560717379fb1db50bc1
Fahad <fullstackdeveloper404@gmail.com>
2022-07-24 11:45:20 +0530

    Code is indented and only favourites page changes are pending

---
+++ JS/sidebar.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "230px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+  function openNava() {
+    document.getElementById("myaccount").style.width = "230px";
+  }
+
+  function closeNava() {
+    document.getElementById("myaccount").style.width = "0";
+  }
